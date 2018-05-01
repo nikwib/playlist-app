54ed6cdd792d58729621dced80867e56b6624b78
Niklas Wibelius <niklas@wibelius.se>
2018-05-01 23:45:43 +0200

    fix: deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config = {
-   baseUrl: 'http://localhost:4000/api/v1',
+  // baseUrl: 'http://localhost:4000/api/v1',
+  baseUrl: 'https://niwi-playlist-api.herokuapp.com/api/v1',
 };
 export default config;
